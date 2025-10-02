01181daeb415587d1f280c3cd45b1ab4a3a1b806
Fermin Isaias Palacios Quiterio <ferminquite01@gmail.com>
2025-10-01 22:23:57 -0600

    v0.0001

--- 3OLIDTS-FerminPalacios-04py/_3OLIDTS_FerminPalacios_04py.py
+++ 3OLIDTS-FerminPalacios-04py/_3OLIDTS_FerminPalacios_04py.py
@@ -1,5 +1,6 @@
 ﻿import tkinter as tk
-from tkinter import Label, messagebox
+from tkinter import messagebox
+import re
 
 def limpiar_campos():
     tbNombre.delete(0,tk.END)
@@ -25,20 +26,45 @@ def guardar_valores():
         genero="hombre"
     elif var_genero.get()==2:
         genero="mujer"
+    
+        ###validar que los campos tengan el formato correcto
+    if (es_entero_valido(edad) and es_decimal_valido(estatura)and es_entero_valido_de_10_digitos(telefono)
+        and es_texto_valido(nombres) and es_texto_valido(apellidos)):
 
-    ###genera la cadena de caracteres
-    datos= "Nombres: "+ nombres + "\n" + "apellidos: " + apellidos + "\n"+"edad"+ edad + "anos\n" + "Estarura: " + estatura +"\n"+"Telefonos: "+telefono+"\n"
-    ###guarda los datos en un archivo txt
-    with open ("3O2025.txt","a") as archivo:
-        archivo.write(datos +"\n\n")
-    ##mostra mensaje de confimarcion
-    messagebox.showinfo("informacion", "datos guardados con exito: \n\n"+ datos )
-    tbNombre.delete(0,tk.END)
-    tbApellidos.delete(0,tk.END)
-    tbEdad.delete(0, tk.END)
-    tbEstatura.delete(0, tk.END)
-    tbTelefono.delete(0, tk.END)
-    var_genero.set(0)
+
+        ###genera la cadena de caracteres
+        datos= ("Nombres: "+ nombres + "\n" + "apellidos: " + apellidos + "\n"+"edad"+ edad + "anos\n" + "Estarura: " 
+                + estatura +"\n"+"Telefonos: "+telefono+"\n")
+        ###guarda los datos en un archivo txt
+        with open ("3O2025.txt","a") as archivo:
+            archivo.write(datos +"\n\n")
+        ##mostra mensaje de confimarcion
+        messagebox.showinfo("informacion", "datos guardados con exito: \n\n"+ datos )
+        limpiar_campos()
+    else:
+        messagebox.showinfo("informacion", "por favor, ingrese datos válidos en los campos.")
+        limpiar_campos()
+
+def es_entero_valido(valor):
+    try:
+        int (valor)
+        return True
+    except ValueError:
+        return False
+
+def es_decimal_valido(valor):
+    try:
+        float(valor)
+        return True
+    except ValueError:
+        return False
+
+def es_entero_valido_de_10_digitos(valor):
+    return valor.isdigit() and len(valor) ==10
+
+def es_texto_valido(valor):
+    return bool(re.match("^[a-zA-Z\s]+$",valor))
+
 
 #creacion ventana
 ventana = tk.Tk()
